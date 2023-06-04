80c8746ecc9234be25d1007b58cbefb3ab2ff279
YuriiPohorilets <pohorilets.y@gmail.com>
2023-06-04 20:58:21 +0300

    add. db connection

--- models/order.js
+++ models/order.js
@@ -1,42 +1,46 @@
 const { model, Schema } = require('mongoose');
 
-const orderSchema = new Schema({
-  productId: {
-    type: String,
-    required: [true, 'Product is required'],
+const orderSchema = new Schema(
+  {
+    productId: {
+      type: String,
+      required: [true, 'Product is required'],
+    },
+
+    shopId: {
+      type: String,
+      required: [true, 'Shop is required'],
+    },
+
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: [true, 'Customer is required'],
+    },
+
+    totalValue: {
+      type: Number,
+      required: [true, 'Total value is required'],
+    },
+
+    amount: {
+      type: Number,
+      required: [true, 'Amount is required'],
+    },
+
+    address: {
+      type: String,
+      required: [true, 'Address is required'],
+    },
+
+    phone: {
+      type: String,
+      required: [true, 'Phone is required'],
+    },
   },
 
-  shopId: {
-    type: String,
-    required: [true, 'Shop is required'],
-  },
-
-  customer: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: [true, 'Customer is required'],
-  },
-
-  totalValue: {
-    type: Number,
-    required: [true, 'Total value is required'],
-  },
-
-  amount: {
-    type: Number,
-    required: [true, 'Amount is required'],
-  },
-
-  address: {
-    type: String,
-    required: [true, 'Address is required'],
-  },
-
-  phone: {
-    type: String,
-    required: [true, 'Phone is required'],
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Order = model('orders', orderSchema);
 
